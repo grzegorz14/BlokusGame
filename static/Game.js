d3a225e41841572a52c41bfb4123caf9b0dccc8c
Witold Niedźwiedź <donwito@outlook.com>
2022-06-14 10:08:36 +0200

    placeEnemy skeleton added

--- static/Game.js
+++ static/Game.js
@@ -149,7 +149,7 @@ class Game {
 
             //removes block button from ui
             try {
-                document.getElementById("your" + this.pickedBlockId).remove()
+                document.getElementById("your" + this.pickedBlockId)?.remove()
                 document.getElementById("yourPoints").innerText = parseInt(document.getElementById("yourPoints").innerText) + pointsCounter
             } catch {
 
@@ -168,190 +168,206 @@ class Game {
         }
     }
 
-    validatePlacement = () => {
+    placeEnemy(coords) {
+        // make new block
+        let block = new Block(Blocks.blocks[id], !this.player, false)
+        this.scene.add(block)
+
+        block.setRotation(coords.rot)
+
+        // position in default rotation on current position
+        block.position.set(
+            coords.x * 10 - 70,
+            0,
+            coords.z * 10 - 70
+        )
+    }
+}
+
+validatePlacement = () => {
+    let w = this.placementHelper.w
+    let h = this.placementHelper.h
+
+    // adjecancy grid
+    let top = 1,
+        bottom = 1,
+        right = 1,
+        left = 1
+    let overflowX = this.placementCoords.x + w - 14
+    let overflowZ = this.placementCoords.z + h - 14
+    if (overflowX > -1) right = 0
+    if (overflowZ > -1) bottom = 0
+    if (this.placementCoords.x == 0) left = 0
+    if (this.placementCoords.z == 0) top = 0
+
+    let doesNotTouch = true
+    let connected = false
+
+    for (let i = 0; i < h + bottom + top; i++) {
+        for (let j = 0; j < w + right + left; j++) {
+            // // top outer
+            // if (top == 1 && i == 0 && j != -1 + left && j != w + left) {
+            //     console.log("Top Outer", i, j)
+            //     row.push(this.placementHelper.shape[i + 1 - top][j - left])
+            // }
+            // // bottom outer
+            // else if (bottom == 1 && i == w + bottom + top - 1 && j != -1 + left && j != w + left) {
+            //     console.log("Bottom Outer", i, j)
+            //     row.push(this.placementHelper.shape[i - 1 - top][j - left])
+            // }
+            // // left outer
+            // else if (left == 1 && j == 0 && i != -1 + top && i != h + top) {
+            //     console.log("Left Outer", i, j)
+            //     row.push(this.placementHelper.shape[i - top][j + 1 - left])
+            // }
+            // // right outer
+            // else if (right == 1 && j == h + right + left - 1 && i != -1 + bottom && i != h + bottom) {
+            //     console.log("Right Outer", i, j)
+            //     row.push(this.placementHelper.shape[i - top][j - 1 - left])
+            // }
+
+            let populate = false
+            if (i < h + top - 1 && j != -1 + left && j != left + w) {
+                if (this.placementHelper.shape[i - top + 1][j - left] == 1)
+                    populate = true
+            }
+            if (i > top && j != -1 + left && j != left + w) {
+                if (this.placementHelper.shape[i - top - 1][j - left] == 1)
+                    populate = true
+            }
+            if (j < w + left - 1 && i != -1 + top && i != top + h) {
+                if (this.placementHelper.shape[i - top][j - left + 1] == 1)
+                    populate = true
+            }
+            if (j > left && i != -1 + top && i != top + h) {
+                if (this.placementHelper.shape[i - top][j - left - 1] == 1)
+                    populate = true
+            }
+
+            if (populate && this.board[this.placementCoords.z + i - top][this.placementCoords.x + j - left] == 1) {
+                doesNotTouch = false
+            }
+        }
+    }
+
+    let isSpaceEmpty = true
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            if (this.placementHelper.shape[i][j] == 1) {
+                if (this.board[this.placementCoords.z + i][this.placementCoords.x + j] != 0) {
+                    isSpaceEmpty = false
+                }
+            }
+        }
+    }
+
+    if (this.firstMove && this.placementCoords.x == 0 && this.placementCoords.z == 14 - h) {
+        connected = true
+
+    }
+    // DEBUG
+    else if (!this.firstMove) {
+        connected = true
+
+    }
+    // DEBUG
+
+    console.log(isSpaceEmpty, doesNotTouch, connected)
+
+    return isSpaceEmpty && doesNotTouch && connected
+}
+
+selectBlock = (id) => {
+    //check if it's your turn
+    if (!this.yourTurn) return
+
+    this.pickedBlockId = id
+
+    // safely delete old block
+    if (this.placementHelper) {
+        this.placementHelper.safeDelete(this.scene)
+    }
+
+    // make new block
+    let block = new Block(Blocks.blocks[id], this.player, true)
+    this.scene.add(block)
+
+    // replace old helper
+    if (this.placementHelper) {
+        let rot = this.placementHelper.rotationID
+        let x = this.placementHelper.position.x
+        let z = this.placementHelper.position.z
         let w = this.placementHelper.w
         let h = this.placementHelper.h
 
-        // adjecancy grid
-        let top = 1,
-            bottom = 1,
-            right = 1,
-            left = 1
+        this.placementHelper = block
+
+        // position in default rotation on current position
+        this.placementHelper.position.set(
+            70 - this.placementCoords.x * 10,
+            0,
+            70 - this.placementCoords.z * 10
+        )
+
+        // restore rotation
+        this.placementHelper.setRotation(rot)
+
+        // Prevent Clipping out of board
         let overflowX = this.placementCoords.x + w - 14
         let overflowZ = this.placementCoords.z + h - 14
-        if (overflowX > -1) right = 0
-        if (overflowZ > -1) bottom = 0
-        if (this.placementCoords.x == 0) left = 0
-        if (this.placementCoords.z == 0) top = 0
-
-        let doesNotTouch = true
-        let connected = false
-
-        for (let i = 0; i < h + bottom + top; i++) {
-            for (let j = 0; j < w + right + left; j++) {
-                // // top outer
-                // if (top == 1 && i == 0 && j != -1 + left && j != w + left) {
-                //     console.log("Top Outer", i, j)
-                //     row.push(this.placementHelper.shape[i + 1 - top][j - left])
-                // }
-                // // bottom outer
-                // else if (bottom == 1 && i == w + bottom + top - 1 && j != -1 + left && j != w + left) {
-                //     console.log("Bottom Outer", i, j)
-                //     row.push(this.placementHelper.shape[i - 1 - top][j - left])
-                // }
-                // // left outer
-                // else if (left == 1 && j == 0 && i != -1 + top && i != h + top) {
-                //     console.log("Left Outer", i, j)
-                //     row.push(this.placementHelper.shape[i - top][j + 1 - left])
-                // }
-                // // right outer
-                // else if (right == 1 && j == h + right + left - 1 && i != -1 + bottom && i != h + bottom) {
-                //     console.log("Right Outer", i, j)
-                //     row.push(this.placementHelper.shape[i - top][j - 1 - left])
-                // }
-
-                let populate = false
-                if (i < h + top - 1 && j != -1 + left && j != left + w) {
-                    if (this.placementHelper.shape[i - top + 1][j - left] == 1)
-                        populate = true
-                }
-                if (i > top && j != -1 + left && j != left + w) {
-                    if (this.placementHelper.shape[i - top - 1][j - left] == 1)
-                        populate = true
-                }
-                if (j < w + left - 1 && i != -1 + top && i != top + h) {
-                    if (this.placementHelper.shape[i - top][j - left + 1] == 1)
-                        populate = true
-                }
-                if (j > left && i != -1 + top && i != top + h) {
-                    if (this.placementHelper.shape[i - top][j - left - 1] == 1)
-                        populate = true
-                }
-
-                if (populate && this.board[this.placementCoords.z + i - top][this.placementCoords.x + j - left] == 1) {
-                    doesNotTouch = false
-                }
-            }
+        if (overflowX > 0) {
+            this.placementHelper.position.x += 10 * overflowX
+            this.placementCoords.x -= overflowX
         }
 
-        let isSpaceEmpty = true
-        for (let i = 0; i < h; i++) {
-            for (let j = 0; j < w; j++) {
-                if (this.placementHelper.shape[i][j] == 1) {
-                    if (this.board[this.placementCoords.z + i][this.placementCoords.x + j] != 0) {
-                        isSpaceEmpty = false
-                    }
-                }
-            }
-        }
-
-        if (this.firstMove && this.placementCoords.x == 0 && this.placementCoords.z == 14 - h) {
-            connected = true
-
-        }
-        // DEBUG
-        else if (!this.firstMove) {
-            connected = true
-
-        }
-        // DEBUG
-
-        console.log(isSpaceEmpty, doesNotTouch, connected)
-
-        return isSpaceEmpty && doesNotTouch && connected
-    }
-
-    selectBlock = (id) => {
-        //check if it's your turn
-        if (!this.yourTurn) return
-
-        this.pickedBlockId = id
-
-        // safely delete old block
-        if (this.placementHelper) {
-            this.placementHelper.safeDelete(this.scene)
-        }
-
-        // make new block
-        let block = new Block(Blocks.blocks[id], this.player, true)
-        this.scene.add(block)
-
-        // replace old helper
-        if (this.placementHelper) {
-            let rot = this.placementHelper.rotationID
-            let x = this.placementHelper.position.x
-            let z = this.placementHelper.position.z
-            let w = this.placementHelper.w
-            let h = this.placementHelper.h
-
-            this.placementHelper = block
-
-            // position in default rotation on current position
-            this.placementHelper.position.set(
-                70 - this.placementCoords.x * 10,
-                0,
-                70 - this.placementCoords.z * 10
-            )
-
-            // restore rotation
-            this.placementHelper.setRotation(rot)
-
-            // Prevent Clipping out of board
-            let overflowX = this.placementCoords.x + w - 14
-            let overflowZ = this.placementCoords.z + h - 14
-            if (overflowX > 0) {
-                this.placementHelper.position.x += 10 * overflowX
-                this.placementCoords.x -= overflowX
-            }
-
-            if (overflowZ > 0) {
-                this.placementHelper.position.z += 10 * overflowZ
-                this.placementCoords.z -= overflowZ
-            }
-        }
-        // make new helper
-        else {
-            this.placementHelper = block
-
-            // position in default rotation on current position
-            this.placementHelper.position.set(
-                70 - this.placementCoords.x * 10,
-                0,
-                70 - this.placementCoords.z * 10
-            )
-        }
-
-        this.placementHelper.placable(this.validatePlacement())
-    }
-
-    createBoard = () => {
-        let tile
-        for (let i = 0; i < 14; i++) {
-            let row = []
-            for (let j = 0; j < 14; j++) {
-                tile = new Tile(i, j)
-                // jak chcesz dalej plansze, to oddal kamere
-                tile.position.set(65 - j * 10, -4, 65 - i * 10)
-                this.tiles.push(tile)
-                this.scene.add(tile)
-                row.push(0)
-            }
-            this.board.push(row)
+        if (overflowZ > 0) {
+            this.placementHelper.position.z += 10 * overflowZ
+            this.placementCoords.z -= overflowZ
         }
     }
+    // make new helper
+    else {
+        this.placementHelper = block
 
-    setPlayerPosition = () => {
-        this.camera.position.set(0, 120, -180)
-        this.camera.lookAt(this.scene.position)
+        // position in default rotation on current position
+        this.placementHelper.position.set(
+            70 - this.placementCoords.x * 10,
+            0,
+            70 - this.placementCoords.z * 10
+        )
     }
 
-    blockButtonClick = (id) => this.selectBlock(id)
+    this.placementHelper.placable(this.validatePlacement())
+}
 
-    render = () => {
-        TWEEN.update()
-        requestAnimationFrame(this.render)
-        this.renderer.render(this.scene, this.camera)
+createBoard = () => {
+    let tile
+    for (let i = 0; i < 14; i++) {
+        let row = []
+        for (let j = 0; j < 14; j++) {
+            tile = new Tile(i, j)
+            // jak chcesz dalej plansze, to oddal kamere
+            tile.position.set(65 - j * 10, -4, 65 - i * 10)
+            this.tiles.push(tile)
+            this.scene.add(tile)
+            row.push(0)
+        }
+        this.board.push(row)
     }
+}
+
+setPlayerPosition = () => {
+    this.camera.position.set(0, 120, -180)
+    this.camera.lookAt(this.scene.position)
+}
+
+blockButtonClick = (id) => this.selectBlock(id)
+
+render = () => {
+    TWEEN.update()
+    requestAnimationFrame(this.render)
+    this.renderer.render(this.scene, this.camera)
+}
 }
 
 export default Game
